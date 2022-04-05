8154e9e1feb37d2c9745fec6de6b79b5c3108ce3
Gustavo Isacura <i.tavo1512@gmail.com>
2022-04-04 22:31:37 -0400

    Culminacion de la seccion main

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,8 @@
 //React
-import React from 'react';
-
+import React, {useRef} from 'react';
+//Fontawesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons'
 //Components
 
 //Styles
@@ -8,18 +10,42 @@ import '../styles/Header.css'
 
 
 const Header = () => {
+  const mobileMenu = useRef(null)
+
+  const toggleShowMenu = () => {
+    mobileMenu.current.classList.toggle('show')
+  }
   return (
     <header>
-      <div className='logo-container'>
-        <img src='../../assets/logo/logo1_small.png' alt='logo'/>
-      </div>
       <nav>
-        <ul>
-          <li>Inicio</li>
-          <li>Sobre Mi</li>
-          <li>Proyectos</li>
-          <li>Contacto</li>
-        </ul>
+        <div className='desktop__menu'>
+          <div className='logo-container'>
+            <img src='../../assets/logo/logo_small1.png' alt='logo'/>
+          </div>
+          <ul>
+            <li>Inicio</li>
+            <li>Sobre Mi</li>
+            <li>Proyectos</li>
+            <li>Contacto</li>
+          </ul>
+        </div>
+
+        <div className='mobile'>
+          <div className='header'>
+            <div className='logo-container'>
+              <img src='../../assets/logo/logo_small1.png' alt='logo'/>
+            </div>
+            <span onClick={toggleShowMenu}>
+              <FontAwesomeIcon className='menu-icon' icon={faBars}/>
+            </span>
+          </div>
+            <ul ref={mobileMenu}>
+              <li>Inicio</li>
+              <li>Sobre Mi</li>
+              <li>Proyectos</li>
+              <li>Contacto</li>
+            </ul>
+        </div>
       </nav>
     </header>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,9 @@
 //React
 import React from 'react';
-
 //Fontawesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
+import { faTwitterSquare, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+//Components
 //Styles
 import '../styles/Home.css'
 
@@ -19,16 +19,20 @@ const Home = () => {
           <h3>Desarrolador Frontend</h3>
           <div className='main__information--social-media'>
             <a href='https://linkedin.com/in/gustavoisacura' target="_blank">
+              {/* <FontAwesomeIcon icon={faLinkedin} className='linkedin-icon'/> */}
+              <img src='../../assets/logo/icons8-linkedin-48.png' alt='Linkedin'/>
               <p>Linkedin</p>
-              <FontAwesomeIcon icon="fa-brands fa-linkedin" />
             </a>
             <a href='https://github.com/GIsacura' target="_blank">
+              {/* <FontAwesomeIcon icon={faGithub} className='github-icon'/> */}
+              <img src='../../assets/logo/icons8-github-48.png' alt='Github'/>
               <p>Github</p>
-              <FontAwesomeIcon icon="fa-brands fa-github" />
             </a>
             <a href='https://twitter.com/gisacura_' target="_blank">
+              {/* <FontAwesomeIcon icon={faTwitterSquare} className='twitter-icon'/> */}
+              <img src='../../assets/logo/icons8-twitter-cuadrado-48.png' alt='Twitter
+              '/>
               <p>Twitter</p>
-              <FontAwesomeIcon icon="fa-brands fa-twitter" />
             </a>
           </div>
         </section>
